e2dfffd619379004248e1c0d4f9be8ce80ececc5
Marina Skuybit <marina.skuybit@q5kxcqpw5p.lan>
2024-04-08 11:36:55 -0400

    Initial commit

---
+++ start0202/test/KeyboardAndMouseInput.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class KeyboardAndMouseInput {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.click();
+        name.sendKeys("Marina Test");
+
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+
+
+        driver.quit();
+    }
+}
---
+++ start0204/test/ScrollToElement.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+
+public class ScrollToElement {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/scroll");
+
+        WebElement name = driver.findElement(By.id("name"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(name);
+        name.sendKeys("Marina Test");
+
+        WebElement date = driver.findElement(By.id("date"));
+        date.sendKeys("o1/o1/2020");
+
+
+        driver.quit();
+    }
+}
